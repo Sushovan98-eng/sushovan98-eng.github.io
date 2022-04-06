e9a4a16980ba0ee1f02f075b61b472c733366351
Sushovan98-eng <ssovan965@gmail.com>
2022-04-06 11:01:15 +0530

    initilize

---
+++ script.js
@@ -0,0 +1,18 @@
+// for navbar section
+const toggleButton = document.getElementsByClassName("navbar-toggle")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links");
+const navbar = document.getElementsByClassName("navbar")[0];
+const banner = document.getElementsByClassName("banner")[0];
+toggleButton.addEventListener("click", () => {
+  for (var i = 0; i < navbarLinks.length; i++)
+    navbarLinks[i].classList.toggle("active");
+  navbar.classList.toggle("active");
+  banner.classList.toggle("banner-hide");
+});
+
+//for search section
+const toggleSearch = document.getElementsByClassName("search-btn")[0];
+const searchBar = document.getElementsByClassName("banner-search")[0];
+toggleSearch.addEventListener("click", () => {
+  searchBar.classList.toggle("active1");
+});
